2a24bdd89b3a37e189c908ae667bd6c0532f3808
SteeringBIG <arway@inbox.ru>
2019-10-22 20:07:20 +0700

    Function

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <?php
+    echo '<pre>';
 
     $mass = array();
-    $massCount = 20;
+    $massCount = 9;
     $nStart = 0;
     $nEnd = 100;
-    $x=0;
-    while ($x++ < $massCount) {
-        $mass[$x] = rand($nStart, $nEnd);
+    $key=0;
+    while ($key++ < $massCount) {
+        $mass[$key] = rand($nStart, $nEnd);
     }
 
+    echo '<br>' . print_r($mass) . '<br>';
 
-    echo print_r($mass);
-function
+    $key = $massCount;
+    $keySort = 1;
+    $i = 0;
+    while ($i++ < $keySort) {
+        $mass = sortArray($mass, $key, $keySort);
+    }
+
+    //echo $massCount;
+    echo '<br>' . print_r($mass);
+
+    echo '</pre>';
+
+    function sortArray($mass, $key, $keySort)
+    {
+        while ($key > $keySort) {
+            //echo '<br>' . $mass[$key];
+            $keyA = $key;
+            $keyB = $key - 1;
+            $a = $mass[$keyA];
+            $b = $mass[$keyB];
+
+            if ($a < $b){
+                $mass[$key] = $b;
+                $mass[$key - 1] = $a;
+            }
+
+            $key = $key - 1;
+        }
+        return $mass;
+    }
 ?>
